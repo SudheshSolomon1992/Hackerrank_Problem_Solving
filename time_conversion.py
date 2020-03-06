bb40834a6b97a132434292a8099123601067f425
Sudhesh Solomon <sudhesh.solomon1992@gmail.com>
2020-03-06 15:29:00 +0530

    Update time_conversion.py

--- time_conversion.py
+++ time_conversion.py
@@ -2,14 +2,14 @@
 
 from datetime import datetime
 
-def counvert_to_24(time):
+def convert_to_24(time):
     time_12 = datetime.strptime(time, '%I:%M:%S%p')
     time_24 = str(time_12).split(' ')[1]
     print (time_24)
 
 def main():
     time_12 = input()
-    counvert_to_24(time_12)
+    convert_to_24(time_12)
 
 if __name__ == "__main__":
     main()
